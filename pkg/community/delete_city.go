c46be7107ffae42f8c7e7d7d2b0cb399a8c5a8c2
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-05-20 16:59:22 +0800

    finish crud to test table

---
+++ pkg/community/delete_city.go
@@ -0,0 +1,25 @@
+package community
+
+import (
+	"github.com/VncntDzn/community-tracker-api/pkg/common/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+type DeleteCityRequestBody struct {
+	Name string `json:"name"`
+}
+
+func (h handler) DeleteCity(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	var city models.Cities
+
+	if result := h.DB.First(&city, id); result.Error != nil {
+		return fiber.NewError(fiber.StatusNotFound, result.Error.Error())
+	}
+
+	// delete city from db
+	h.DB.Delete(&city)
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": "success", "message": "Deleted", "data": &city})
+}

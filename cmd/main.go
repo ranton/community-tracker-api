fda6454d576dba94cea2c742e5fa9be587474abc
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-05-25 19:34:16 +0800

    added cors

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,19 @@ import (
 	"github.com/VncntDzn/community-tracker-api/pkg/cities"
 	"github.com/VncntDzn/community-tracker-api/pkg/common/db"
 	"github.com/VncntDzn/community-tracker-api/pkg/community"
+	"github.com/VncntDzn/community-tracker-api/pkg/community_managers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
 	myDB := db.Init()
+	app.Use(cors.New())
 
 	community.RegisterRoutes(app, myDB)
 	cities.RegisterRoutes(app, myDB)
-
+	community_managers.RegisterRoutes(app, myDB)
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusOK).SendString(":8000")
 	})

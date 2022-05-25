22ed00de30dcac81d3f15d1dbac8d3ca8b612438
Ken Suyat <940541@cognizant.com>
2022-05-25 16:47:39 +0800

    Added Community Icons Updated Created DB Scripts Add Populate DB Tables Scripts

--- postgresql/Create DB Tables.sql
+++ postgresql/Create DB Tables.sql
@@ -8,8 +8,8 @@ CREATE TABLE public.community (
 	communityid int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	communityname varchar(50) NOT NULL,
 	image bytea NULL,
-	colortheme varchar(30) NULL,
-	isactive bool NOT NULL,
+--	colortheme varchar(30) NULL,
+--	isactive bool NOT NULL,
 	communitymgrid int4 NULL,
 	CONSTRAINT community_pkey PRIMARY KEY (communityid)
 );

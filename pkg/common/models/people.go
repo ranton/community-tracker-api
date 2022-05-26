cc6d857c0771b0ba4036fb766f8fa7dcb5de53ed
Kenneth Russel Casares <944527@cognizant.com>
2022-05-26 16:44:23 +0800

    ct-27

--- pkg/common/models/people.go
+++ pkg/common/models/people.go
@@ -20,17 +20,16 @@ func (People) TableName() string {
 }
 
 type AddPeople struct {
-	Cognizantid int    `gorm:"column:cognizantid" json:"cognizantid_id"`
-	Lastname    string `gorm:"column:lastname" json:"last_name"`
-	Firstname   string `gorm:"column:firstname" json:"first_name"`
-	Middlename  string `gorm:"column:middlename" json:"middle_name"`
-	Fullname    string `gorm:"column:fullname" json:"full_name"`
-	Csvemail    string `gorm:"column:csvemail" json:"csv_email"`
-	Hireddate   string `gorm:"column:hireddate" json:"hired_date"`
-	Workstateid int    `gorm:"column:workstateid" json:"workstate_id"`
-	Joblevelid  int    `gorm:"column:joblevelid" json:"joblevel_id"`
-	Projectid   int    `gorm:"column:projectid" json:"project_id"`
-	Isactive    bool   `gorm:"column:isactive" json:"is_active"`
+	PeopleID    string `gorm:"column:peopleid" json:"people_id"`
+	CognizantID int    `gorm:"column:cognizantid" json:"cognizant_id"`
+	LastName    string `gorm:"column:lastname" json:"last_name"`
+	FirstName   string `gorm:"column:firstname" json:"first_name"`
+	MiddleName  string `gorm:"column:middlename" json:"middle_name"`
+	HiredDate   string `gorm:"column:hireddate" json:"hired_date"`
+	CSVEmail    string `gorm:"column:csvemail" json:"csv_email"`
+	JobLevel    int    `gorm:"column:joblevelid" json:"job_level_id"`
+	CommunityID string `gorm:"column:communityid" json:"community_id"`
+	IsActive    bool   `gorm:"column:isactive" json:"is_active"`
 }
 
 func (AddPeople) TableName() string {
@@ -38,18 +37,16 @@ func (AddPeople) TableName() string {
 }
 
 type UpdatePeople struct {
-	Peopleid    int    `gorm:"column:peopleid" json:"people_id"`
-	Cognizantid int    `gorm:"column:cognizantid" json:"cognizantid_id"`
-	Lastname    string `gorm:"column:lastname" json:"last_name"`
-	Firstname   string `gorm:"column:firstname" json:"first_name"`
-	Middlename  string `gorm:"column:middlename" json:"middle_name"`
-	Fullname    string `gorm:"column:fullname" json:"full_name"`
-	Csvemail    string `gorm:"column:csvemail" json:"csv_email"`
-	Hireddate   string `gorm:"column:hireddate" json:"hired_date"`
-	Workstateid int    `gorm:"column:workstateid" json:"workstate_id"`
-	Joblevelid  int    `gorm:"column:joblevelid" json:"joblevel_id"`
-	Projectid   int    `gorm:"column:projectid" json:"project_id"`
-	Isactive    bool   `gorm:"column:isactive" json:"is_active"`
+	PeopleID    string `gorm:"column:peopleid" json:"people_id"`
+	CognizantID int    `gorm:"column:cognizantid" json:"cognizant_id"`
+	LastName    string `gorm:"column:lastname" json:"last_name"`
+	FirstName   string `gorm:"column:firstname" json:"first_name"`
+	MiddleName  string `gorm:"column:middlename" json:"middle_name"`
+	HiredDate   string `gorm:"column:hireddate" json:"hired_date"`
+	CSVEmail    string `gorm:"column:csvemail" json:"csv_email"`
+	JobLevel    int    `gorm:"column:joblevelid" json:"job_level_id"`
+	CommunityID string `gorm:"column:communityid" json:"community_id"`
+	IsActive    bool   `gorm:"column:isactive" json:"is_active"`
 }
 
 func (UpdatePeople) TableName() string {

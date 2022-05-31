2bc8f13bea7076b453b275564fe5c770163bb894
Arvin De Los Santos <arvin.delossantos@softvision.com>
2022-05-31 18:54:48 +0800

    Created community members
    Enpoint to get members using community id

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/VncntDzn/community-tracker-api/pkg/common/db"
 	"github.com/VncntDzn/community-tracker-api/pkg/community"
 	"github.com/VncntDzn/community-tracker-api/pkg/community_managers"
+	"github.com/VncntDzn/community-tracker-api/pkg/community_members"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -20,6 +21,7 @@ func main() {
 	cities.RegisterRoutes(app, myDB)
 	community_managers.RegisterRoutes(app, myDB)
 	people.RegisterRoutes(app, myDB)
+	community_members.RegisterRoutes(app, myDB)
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusOK).SendString(":8000")
---
+++ pkg/community_members/get_community_members.go
@@ -0,0 +1,30 @@
+package community_members
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/VncntDzn/community-tracker-api/pkg/common/models"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func (h handler) GetCommunityMembers(c *fiber.Ctx) error {
+	var community_data models.CommunityMembers
+	communityId := c.Params("community")
+
+	result := h.DB.Where(&models.CommunityMembers{CommunityID: communityId}).Preload("Members").First(&community_data)
+
+	// show 404 error if no community found
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		fmt.Print(result.Error)
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"status": fiber.StatusNotFound, "message": "Community does not exist."})
+	}
+
+	// check for other errors
+	if result.Error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "message": "Server error."})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": fiber.StatusOK, "message": "Success!", "data": &community_data})
+}

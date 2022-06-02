92087aea4879c8bc5a1947eab48320e2e8833e1d
Kenneth Russel Casares <944527@cognizant.com>
2022-06-02 18:22:58 +0800

    commit

--- pkg/common/db/db.go
+++ pkg/common/db/db.go
@@ -15,8 +15,6 @@ func Init() *gorm.DB {
 	DB_NAME := config.GetEnv("DB_NAME")
 	DB_USERNAME := config.GetEnv("DB_USERNAME")
 
-	fmt.Printf(config.GetEnv("DB_HOST"))
-
 	DB_PASSWORD := config.GetEnv("DB_PASSWORD")
 	postgresConnection := fmt.Sprintf(
 		"host=%s  port=%s   user=%s   password=%s  dbname=%s  sslmode=disable",

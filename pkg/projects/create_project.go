0b229a5ab2a106595c46bb4a0b42584eb2823b6e
Arvin De Los Santos <arvin.delossantos@softvision.com>
2022-06-22 16:04:59 +0800

    Added validation for project name

--- pkg/projects/create_project.go
+++ pkg/projects/create_project.go
@@ -1,10 +1,14 @@
 package projects
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/VncntDzn/community-tracker-api/pkg/common/models"
 	requests "github.com/VncntDzn/community-tracker-api/pkg/projects/requests"
 	projectValidation "github.com/VncntDzn/community-tracker-api/pkg/validations/projects"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func (h handler) CreateProject(c *fiber.Ctx) error {
@@ -23,6 +27,17 @@ func (h handler) CreateProject(c *fiber.Ctx) error {
 		ProjectName: projectRequest.ProjectName,
 		ProjectLead: projectRequest.ProjectLead,
 		IsActive:    true,
+	}
+
+	project := &models.Project{}
+	uppercaseProjectName := strings.ToUpper(projectRequest.ProjectName)
+	projectSearchErr := h.DB.Where("UPPER(projectdesc) = ?", uppercaseProjectName).First(&project).Error
+	if projectSearchErr != nil && !errors.Is(projectSearchErr, gorm.ErrRecordNotFound) {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "error": projectSearchErr.Error()})
+	}
+
+	if project.ProjectId != 0 {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"status": fiber.StatusUnprocessableEntity, "error": "Project already exists."})
 	}
 
 	projectResult := h.DB.Create(&projectData)

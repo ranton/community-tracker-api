1e6f81f58e89c535d308dd608897a02ccebeba61
Arvin De Los Santos <arvin.delossantos@softvision.com>
2022-06-28 16:24:13 +0800

    Created and endpoint to store admin details

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/VncntDzn/community-tracker-api/pkg/admin"
 	"github.com/VncntDzn/community-tracker-api/pkg/cities"
 	"github.com/VncntDzn/community-tracker-api/pkg/member_skills"
 	"github.com/VncntDzn/community-tracker-api/pkg/people"
@@ -26,6 +27,7 @@ func main() {
 	community_members.RegisterRoutes(app, myDB)
 	projects.RegisterRoutes(app, myDB)
 	member_skills.RegisterRoutes(app, myDB)
+	admin.RegisterRoutes(app, myDB)
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusOK).SendString(":8000")
---
+++ pkg/admin/controller.go
@@ -0,0 +1,18 @@
+package admin
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type handler struct {
+	DB *gorm.DB
+}
+
+func RegisterRoutes(app *fiber.App, db *gorm.DB) {
+	h := &handler{
+		DB: db,
+	}
+	route := app.Group("/api/admin")
+	route.Post("/", h.CreateAdmin)
+}

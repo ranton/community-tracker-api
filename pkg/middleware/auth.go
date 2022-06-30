13dde47ff4ba8c159586b5887a47cc5893c0b232
Arvin De Los Santos <arvin.delossantos@softvision.com>
2022-06-30 14:11:59 +0800

    Created middleware to handle authorization

---
+++ pkg/middleware/auth.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/VncntDzn/community-tracker-api/config"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthMiddleware(ctx *fiber.Ctx) error {
+	authHeader := ctx.Get("Authorization")
+	authFields := strings.Fields(authHeader)
+
+	if authFields[0] != "Bearer" {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": fiber.StatusUnauthorized, "message": "Unathorized Request."})
+	}
+
+	token := authFields[1]
+	_, tErr := jwt.ParseWithClaims(token, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(config.GetEnv("JWT_SECRET")), nil
+	})
+
+	if tErr != nil {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": fiber.StatusUnauthorized, "message": "Unathorized Request."})
+	}
+
+	return ctx.Next()
+}

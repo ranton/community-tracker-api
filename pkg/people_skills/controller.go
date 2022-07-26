6130346f7e98f3db86abcf5bd205e67b6e188b0c
Kevin Jasper Bahia <kevinjasper.bahia@softvision.com>
2022-07-26 14:51:47 +0800

    feat(CT-358) remove all auth guards

--- pkg/people_skills/controller.go
+++ pkg/people_skills/controller.go
@@ -2,7 +2,6 @@ package people_skills
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/VncntDzn/community-tracker-api/pkg/middleware"
 	"gorm.io/gorm"
 )
 
@@ -15,8 +14,8 @@ func RegisterRoutes(app *fiber.App, db *gorm.DB) {
 		DB: db,
 	}
 	route := app.Group("/api/peopleskills")
-	route.Post("/", middleware.AuthMiddleware, h.AddPeopleSkills)
+	route.Post("/", h.AddPeopleSkills)
 	route.Get("/", h.GetPeopleSkills)
-	route.Put("/:peopleskillsid", middleware.AuthMiddleware, h.UpdateSkill)
-	route.Delete("/:peopleskillsid", middleware.AuthMiddleware, h.DeleteSkills)
+	route.Put("/:peopleskillsid", h.UpdateSkill)
+	route.Delete("/:peopleskillsid", h.DeleteSkills)
 }

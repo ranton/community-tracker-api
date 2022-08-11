a4ac12eb31855c45e882d18fd015c7622af52ba4
Kevin Jasper Bahia <kevinjasper.bahia@softvision.com>
2022-08-11 15:54:45 +0800

    feat(CT-358) add additional guards

--- pkg/projects/controller.go
+++ pkg/projects/controller.go
@@ -17,8 +17,8 @@ func RegisterRoutes(app *fiber.App, db *gorm.DB) {
 
 	projectRoutes := app.Group("/api/projects")
 	projectRoutes.Post("/", middleware.AuthMiddleware, h.CreateProject)
-	projectRoutes.Put("/:projectid", h.UpdateProject)
+	projectRoutes.Put("/:projectid", middleware.AuthMiddleware, h.UpdateProject)
 	projectRoutes.Get("/", h.GetProjects)
 	projectRoutes.Get("/:projectid", h.GetProject)
-	projectRoutes.Delete("/:projectid", h.DeleteProject)
+	projectRoutes.Delete("/:projectid", middleware.AuthMiddleware, h.DeleteProject)
 }
